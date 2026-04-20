b05456b51b8d863bf3acb1cb4914623a08ff439e
jerrywang355 <89384883+jerrywang355@users.noreply.github.com>
2026-04-20 04:15:35 -0400

    Fix GitHub Pages project-path home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,8 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/us-university-report-site" component={Home} />
+      <Route path="/us-university-report-site/" component={Home} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
